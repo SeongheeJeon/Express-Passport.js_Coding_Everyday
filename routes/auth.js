afded6b649df7d0aa157dbecb883c9f3b1fecfdf
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-03-24 02:12:21 +0900

    commit all

---
+++ routes/auth.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+var fs = require("fs");
+var sanitizeHtml = require("sanitize-html");
+var template = require("../lib/template.js");
+
+module.exports = function (passport) {
+  router.get("/login", function (request, response) {
+    var fmsg = request.flash();
+    var feedback = "";
+    if (fmsg.message) {
+      feedback = fmsg.message;
+    }
+    var title = "WEB - login";
+    var list = template.list(request.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
+      <div style="color:red">${feedback}</div>
+      <form action="/auth/login_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p>
+          <input type="submit" value="login">
+        </p>
+      </form>
+    `,
+      ""
+    );
+    response.send(html);
+  });
+
+  // app.post(
+  //   "/auth/login_process",
+  //   passport.authenticate("local", {
+  //     successRedirect: "/",
+  //     failureRedirect: "/auth/login",
+  //     failureFlash: true,
+  //   })
+  // );
+
+  router.post("/login_process", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+      if (req.session.flash) {
+        req.session.flash = {};
+      }
+
+      req.flash("message", info.message);
+
+      req.session.save(() => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return res.redirect("/auth/login");
+        }
+
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          return req.session.save(() => {
+            res.redirect("/");
+          });
+        });
+      });
+    })(req, res, next);
+  });
+
+  router.get("/logout", function (request, response) {
+    request.logout();
+    request.session.save(function () {
+      response.redirect("/");
+    });
+  });
+
+  return router;
+};
